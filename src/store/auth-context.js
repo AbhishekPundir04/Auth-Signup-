c3d18ef27c8c1ce5bb3b9ecbd6589a9eeec12286
Abhishek Pundir <122037868+AbhishekPundir04@users.noreply.github.com>
2023-04-06 14:52:45 +0530

    Add files via upload

--- src/store/auth-context.js
+++ src/store/auth-context.js
@@ -1,34 +1,39 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 
 const AuthContext = React.createContext({
-    token:'',
-    isLoggedIn: false,
-    login: (token) =>{},
-    logout: () =>{} 
+  token: "",
+  isLoggedIn: false,
+  login: (token) => {},
+  logout: () => {},
+});
 
-})
+export const AuthContextProvider = (props) => {
+  const initialToken = localStorage.getItem("token");
+  const [token, setToken] = useState(initialToken);
 
-export const AuthContextProvider = props =>{
-const [token, setToken] = useState(null)
+  const userIsLoggedIn = !!token;
 
-    const userIsLoggedIn = !!token;
+  const loginHandler = (token) => {
+    setToken(token);
+    localStorage.setItem("token", token);
+  };
+  const logoutHandler = () => {
+    setToken(null);
+    localStorage.removeItem("token");
+  };
 
-const loginHandler = (token) =>{
-    setToken(token)
-}
-const logoutHandler = () =>{
-    setToken(null)
-}
-
-const contextValue = {
+  const contextValue = {
     token: token,
     isLoggedIn: userIsLoggedIn,
     login: loginHandler,
     logout: logoutHandler,
-}
+  };
 
+  return (
+    <AuthContext.Provider value={contextValue}>
+      {props.children}
+    </AuthContext.Provider>
+  );
+};
 
-    return <AuthContext.Provider value={contextValue}>{props.children}</AuthContext.Provider>
-}
-
-export default AuthContext
+export default AuthContext;
